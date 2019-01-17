ae188eea1a8ca64506130bf81427632fa2891771
Daniil Gorbach <daniilhorbach@gmail.com>
2019-01-17 15:59:05 +0300

    repair gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,34 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
 
-// Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
-    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
-        .pipe(sass())
-        .pipe(gulp.dest("src/css"))
-        .pipe(browserSync.stream());
+gulp.task('sass',  function() {
+  return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+    .pipe(sass())
+    .pipe(gulp.dest('src/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
-// Move the javascript files into our /src/js folder
 gulp.task('js', function() {
     return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/tether/dist/js/tether.min.js'])
         .pipe(gulp.dest("src/js"))
-        .pipe(browserSync.stream());
+        .pipe(browserSync.stream())
+        .pipe(browserSync.reload({
+          stream: true
+        }))
 });
 
-// Static Server + watching scss/html files
-gulp.task('serve', gulp.series('sass', function() { 
+gulp.task('serve', function() {
 
-    browserSync.init({
-        server: "./src"  
-    });
+  browserSync.init({
+      server: "./src"
+  });
+  gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'],  gulp.series('sass')); 
+  gulp.watch(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/tether/dist/js/tether.min.js'],  gulp.series('js')); 
 
-    //gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss']);
-    gulp.task('watch', function() {
-        gulp.watch('node_modules/bootstrap/scss/bootstrap.scss', ['sass']);
-        gulp.watch('src/scss/*.scss', ['sass']);
-      });
-    gulp.watch("src/*.html").on('change', browserSync.reload);
-}));
+  gulp.watch("src/*.html").on('change', browserSync.reload);
+});
 
-//gulp.task('default', );
-gulp.task('default', gulp.series(['js','serve'], function() { 
-    // default task code here
-}));
+gulp.task('default', gulp.series(['sass', 'serve'], function() {}));
