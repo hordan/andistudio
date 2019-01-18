de21b106e5b2c483ba314b5cbda254f5d4794bdd
Daniil Gorbach <daniilhorbach@gmail.com>
2019-01-18 18:10:18 +0300

    changes in gulpfile and creating cards

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
+var newer = require('gulp-newer');
+var imagemin = require('gulp-imagemin');
+var inlineimg = require('gulp-inline-images');
+var runSequence = require('run-sequence');
 
 gulp.task('sass',  function() {
   return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
@@ -20,6 +24,12 @@ gulp.task('js', function() {
         }))
 });
 
+gulp.task('inline-images', function(){
+  return gulp.src('../src/img/**/*.+(png|jpg|jpeg|gif|svg)')
+  .pipe(inlineimg('src'))
+  .pipe(gulp.dest('src/img'));
+});
+
 gulp.task('serve', function() {
 
   browserSync.init({
@@ -27,8 +37,10 @@ gulp.task('serve', function() {
   });
   gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'],  gulp.series('sass')); 
   gulp.watch(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/tether/dist/js/tether.min.js'],  gulp.series('js')); 
+  gulp.watch('../src/img/**/*',  gulp.series('inline-images')); 
 
   gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
-gulp.task('default', gulp.series(['sass', 'serve'], function() {}));
+gulp.task('default', gulp.series(['sass' ,'inline-images',   'serve'], function() {}));
+
